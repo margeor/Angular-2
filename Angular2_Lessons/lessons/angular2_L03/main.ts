daf888d258e51a329c6d7b8551dbb0bb2ebbbe1c
margeor <maras.georg@gmail.com>
2015-08-27 16:27:13 +0300

    Angular2_Lessons
    Angular2 Lessons, play and learn

---
+++ Angular2_Lessons/lessons/angular2_L03/main.ts
@@ -0,0 +1,18 @@
+/// <reference path="../../accets/lib/angular2/angular2.d.ts" />
+
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+  selector: 'my-app'
+})
+@View({
+  template: '<h1>Lesson {{1+2}} App {{ yupiText }}</h1>'
+})
+class AppComponent {
+  yupiText: string;
+
+  constructor() {
+      this.yupiText = ', Yupii  !!!';
+  }
+}
+bootstrap(AppComponent);
